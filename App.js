d361809bdc26468a90bb9d80d4b8ca8981693295
IvanR-CS <ivanr@cloudstaff.com>
2025-03-25 14:27:14 +0800

    Integrate Redux for theme management and load theme on app startup

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 // App.js
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { ThemeProvider } from './context/ThemeContext';
-import RootStack from './navigation/RootStack';
+import { Provider } from 'react-redux';
+import { loadTheme } from './src/redux/ThemeSlice';
+import store from './src/redux/Store';
+import RootStack from './src/navigation/RootStack';
 
-export default function App() {
+const App = () => {
+    useEffect(() => {
+        // Load the theme from AsyncStorage when app starts
+        store.dispatch(loadTheme());
+    }, []);
+    
     return (
-        <ThemeProvider>
+        <Provider store={store}>r
             <NavigationContainer>
                 <RootStack />
             </NavigationContainer>
-        </ThemeProvider>
+        </Provider>
     );
-}
+};
+
+export default App;
